bb47a0205e9456dc31c800e83aee4b6eab9ce6d8
Anirudha Pimpale <pimpale.anirudha1994@gmail.com>
2018-09-25 16:13:35 -0400

    fixed css for all pages

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,34 +1,47 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLink } from '@fortawesome/free-solid-svg-icons';
+import { faLink, faThumbsDown, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 
 import './Link.css';
 
 
 class Link extends Component {
-
-	constructor(props) {
-		super(props);
-		this.state = props
-	}
-
 	render() {
+		let score = this.props.Points;
+		let scoreString = ""
+		if(score > 0){
+			scoreString = "+" + this.props.Points.toString()
+		}
+		else scoreString = this.props.Points.toString()
+		let color = 'black'
+		if(this.props.VoteRecord > 0) {
+			color = 'green'
+		}
+		if(this.props.VoteRecord < 0) {
+			color = 'red'
+		}
+
 		return(
 			<div className="showLink">
-				<div id="voteStatus">
-
+				<div className="pointsDisplay">
+					{scoreString}
 				</div>
-				<div id="linkInfo">
-					<h2>{this.state.Name}</h2>
-					<p>
-						<a href="#" onClick={()=> window.open("https://" + this.state.Link, "_blank")}>
-							{this.state.LinkName}
-							<span className="linkIcon">
-								<FontAwesomeIcon icon={faLink} />
-							</span>
-							
-						</a>
-					</p>
+				<div className="voteStatus">
+					<FontAwesomeIcon icon={faThumbsUp} style={{ color: color}}/>
+					<br/>
+					<FontAwesomeIcon icon={faThumbsDown} />
+				</div>
+			
+				<div className="linkInfo">
+					<h2>{this.props.Name}</h2>
+					<a className="mainLink" onClick={()=> window.open("https://" + this.props.Link, "_blank")}>
+						<p className="linkWrap">
+							{this.props.LinkName}		
+						</p>
+						<p className="linkIcon">
+							<FontAwesomeIcon icon={faLink} />
+						</p> 
+					</a>
 				</div>
 				
 			</div>
--- src/components/ShowLinks.js
+++ src/components/ShowLinks.js
@@ -5,17 +5,18 @@ import './ShowLinks.css';
 import Link from './Link';
 
 class ShowLinks extends Component {
-    render() {
-        return(
-            <div className="showLinks container">
-                <h1>Link</h1>
-                <div className="displayBox">
-                    <Link Name="Ani" Link="//www.google.com" LinkName="Google"/>
-                    <Link Name="Ani2" Link="//stackoverflow.com" LinkName="StackOverFlow" />
-                </div>
-            </div>
-        );
-    }
+	render() {
+		return(
+			<div className="showLinks container">
+				<h1>Link</h1>
+				<div className="displayBox">
+					<Link Points={-100} VoteRecord={1} Name="Ani" Link="//www.google.com" LinkName="Google"/>
+					<Link Points={20000} VoteRecord={0} Name="Ani2" Link="//stackoverflow.com" LinkName="StackOverflow" />
+					<Link Points={30000} VoteRecord={-1} Name="Ani3" Link="//facebook.com" LinkName="Facebook" />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default ShowLinks;
